94832bcf58320824a2f9e703056335defbc43ace
Malvin Wijaya <malvinw123@gmail.com>
2023-12-05 23:02:25 +0700

    membuat controller page

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="image.css" />
+  </head>
+  <body>
+    <div class="home-page">
+      <div class="div">
+        <div class="text-wrapper">PT. Lore m Ipsum</div>
+        <p class="lorem-ipsum-lormem">
+          lorem ipsum lormem ipsum dolor sit amet<br/>lorem ipsum lorem ipsum lorem ips<br />olorem ipsum lorem ipsm
+        </p>
+        <div class="lorem-ipsum-lorem">Lorem Ipsum<br/>Lorem</div>
+        <div class="rectangle"></div>
+        <div class="rectangle-2"></div>
+
+        <!-- Contact Us Section -->
+        <div class="overlap">
+          <div class="rectangle-3"></div>
+          <div class="text-wrapper-2">Contact Us</div>
+        </div>
+
+        <!-- Navigation Section -->
+        <div class="overlap-group">
+          <div class="frame">
+            <div class="rectangle-4"></div>
+            <img class="image" src="img/image-3.png" />
+          </div>
+          <div class="menu">
+            <div class="text-wrapper-3">Home</div>
+            <div class="text-wrapper-4">Product</div>
+            <div class="text-wrapper-5">About Us</div>
+          </div>
+          <div class="group">
+            <div class="overlap-2">
+              <div class="div-wrapper"><div class="text-wrapper-6">Search</div></div>
+              <div class="overlap-group-2">
+                <img class="vector" src="img/image.svg" /> <img class="img" src="img/vector.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Footer Section -->
+        <div class="overlap-3">
+          <img class="image-2" src="img/image-7.png" />
+          <div class="frame-2">
+            <div class="text-wrapper-7">Home</div>
+            <div class="text-wrapper-7">Product</div>
+            <div class="text-wrapper-7">About Us</div>
+          </div>
+          <div class="frame-3">
+            <div class="frame-4">
+              <div class="round-place"><img class="shape" src="img/shape.svg" /></div>
+              <p class="text-wrapper-8">Jl. lorem lorem lorem, ipsum ipsum</p>
+            </div>
+            <div class="frame-5">
+              <img class="rounded" src="img/rounded-1.png" />
+              <div class="text-wrapper-8">082111 111 1111 111</div>
+            </div>
+            <div class="frame-6">
+              <img class="mail-icon" src="img/mail-icon.png" />
+              <div class="text-wrapper-8">Lorem@gmail.com</div>
+            </div>
+          </div>
+          <img class="frame-7" src="img/frame-185.svg" />
+          <p class="p">Copyright © 2023 crevon.id. All Rights Reserved</p>
+        </div>
+
+        <!-- Product Carousel Section -->
+        <div class="overlap-4">
+          <div class="text-wrapper-9">BROWSE PRODUCT</div>
+          <div class="carousel-container" id="wrapper">
+            <div class="carousel-items">
+                <div class="carousel-item">
+                    <img src="image/1.jpeg" alt="Image 1">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/2.jpeg" alt="Image 2">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/3.jpeg" alt="Image 3">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/4.jpg" alt="Image 4">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/5.jpg" alt="Image 5">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/6.jpg" alt="Image 6">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/7.jpg" alt="Image 7">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/8.jpg" alt="Image 8">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/9.jpg" alt="Image 9">
+                </div>
+            </div>
+        </div>
+          <div class="rectangle-8"></div>
+        </div>
+
+        <!-- Partner Section -->
+        <div class="overlap-5">
+          <div class="beneficios">
+            <div class="overlap-6">
+              <div class="rectangle-9"></div>
+              <div class="rectangle-10"></div>
+            </div>
+            <div class="overlap-7">
+              <div class="rectangle-9"></div>
+              <div class="rectangle-10"></div>
+            </div>
+            <div class="overlap-group-3">
+              <div class="rectangle-9"></div>
+              <div class="rectangle-11"></div>
+            </div>
+            <p class="our-partner">
+              <span class="span">Our<br /></span> 
+              <span class="text-wrapper-11">Partner</span>
+            </p>
+          </div>
+
+          <!-- Partner Section -->
+          <img class="image-3" src="img/image-9.png" />
+          <img class="image-4" src="img/image.png" />
+          <img class="LOGO-ok-copy" src="img/LOGO-128plus-ok-copy-1699847646259-1.png" />
+        </div>
+
+        <!-- About Us Section -->
+        <div class="overlap-8">
+          <div class="text-wrapper-12">About Us</div>
+          <div class="text-wrapper-13">Our history</div>
+        </div>
+
+        <div class="overlap-9">
+        <div class="text-wrapper-14">Brochures</div>
+      </div>
+    </div>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/Draggable.min.js"></script>
+    <script src="drag.js"></script>
+    <script src="mouse.js"></script>
+  </body>
+</html>
+
